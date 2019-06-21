1c05a7c34560a52bb2cd0c398d97efec9aca3c01
Aykut Bulgu <mabulgu@gmail.com>
2019-06-21 16:44:11 +0300

    fix command

--- strimzi-training/getting-started-with-strimzi/initCourse.sh
+++ strimzi-training/getting-started-with-strimzi/initCourse.sh
@@ -1,9 +1,3 @@
-wget https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.12.0/strimzi-0.12.0.tar.gz
-
-tar -zxvf strimzi-0.12.0.tar.gz
-
-rm -rf strimzi-0.12.0.tar.gz
-
-cd strimzi-0.12.0
+wget https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.12.0/strimzi-0.12.0.tar.gz && tar -zxvf strimzi-0.12.0.tar.gz && rm -rf strimzi-0.12.0.tar.gz && cd strimzi-0.12.0
 
 #git clone system craftsman strimzi training
