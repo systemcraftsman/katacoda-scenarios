b781ac429ffdf5ae6a903b405ad3020c3d5ad2bc
Aykut Bulgu <mabulgu@gmail.com>
2019-06-21 16:26:47 +0300

    fix wrong commands

--- strimzi-training/getting-started-with-strimzi/initCourse.sh
+++ strimzi-training/getting-started-with-strimzi/initCourse.sh
@@ -1,6 +1,6 @@
 wget https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.12.0/strimzi-0.12.0.tar.gz
 
-tar -zxvf https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.12.0/strimzi-0.12.0.tar.gz
+tar -zxvf strimzi-0.12.0.tar.gz
 
 rm -rf strimzi-0.12.0.tar.gz
 
