58c2692f579108653aa28124e0ab7d0a92a42423
Aykut Bulgu <mabulgu@gmail.com>
2019-06-21 16:19:29 +0300

    fix typo

--- strimzi-training/getting-started-with-strimzi/initCourse.sh
+++ strimzi-training/getting-started-with-strimzi/initCourse.sh
@@ -1,6 +1,9 @@
 wget https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.12.0/strimzi-0.12.0.tar.gz
 
-tar -xzvf https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.12.0/strimzi-0.12.0.tar.gz
+tar -zxvf https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.12.0/strimzi-0.12.0.tar.gz
 
+rm -rf strimzi-0.12.0.tar.gz
+
+cd strimzi-0.12.0
 
 #git clone system craftsman strimzi training
