7a49a8fec65cc4994e34f97a6e327e82fb1f1a50
Aykut Bulgu <mabulgu@gmail.com>
2019-06-21 16:16:55 +0300

    download strimzi 0.12.0

--- strimzi-training/getting-started-with-strimzi/initCourse.sh
+++ strimzi-training/getting-started-with-strimzi/initCourse.sh
@@ -1,1 +1,9 @@
-git clone https://github.com/systemcraftsman/strimzi-training
+wget https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.12.0/strimzi-0.12.0.tar.gz
+
+tar -xzvf https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.12.0/strimzi-0.12.0.tar.gz
+
+rm -rf strimzi-0.12.0.tar.gz
+
+cd strimzi-0.12.0
+
+#git clone system craftsman strimzi training
