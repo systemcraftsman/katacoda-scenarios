0868042af3b1c1613ca7872b76399e5bdadd20ee
Aykut Bulgu <mabulgu@gmail.com>
2019-06-21 17:11:50 +0300

    add status indicator

--- strimzi-training/getting-started-with-strimzi/initCourse.sh
+++ strimzi-training/getting-started-with-strimzi/initCourse.sh
@@ -1,3 +1,11 @@
-wget https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.12.0/strimzi-0.12.0.tar.gz && tar -zxvf strimzi-0.12.0.tar.gz && rm -rf strimzi-0.12.0.tar.gz && cd strimzi-0.12.0
+wget https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.12.0/strimzi-0.12.0.tar.gz && tar -zxvf strimzi-0.12.0.tar.gz && rm -rf strimzi-0.12.0.tar.gz && cd strimzi-0.12.0 &
+PID=$!
+i=1
+sp="/-\|"
+echo -n ' '
+while [ -d /proc/$PID ]
+do
+  printf "\b${sp:i++%${#sp}:1}"
+done
 
 #git clone system craftsman strimzi training
