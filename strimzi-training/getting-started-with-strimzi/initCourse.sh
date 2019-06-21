f4cb8cc71af19b6d78396ce8d771af1dc0942a70
Aykut Bulgu <mabulgu@gmail.com>
2019-06-21 17:17:25 +0300

    env -> initials

--- strimzi-training/getting-started-with-strimzi/initCourse.sh
+++ strimzi-training/getting-started-with-strimzi/initCourse.sh
@@ -1,7 +1,7 @@
-echo "Preparing environment..." && 
+echo "Preparing initials..." && 
 wget https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.12.0/strimzi-0.12.0.tar.gz > /dev/null 2>&1 && 
 tar -zxvf strimzi-0.12.0.tar.gz > /dev/null 2>&1 && 
 rm -rf strimzi-0.12.0.tar.gz > /dev/null 2>&1 && 
 cd strimzi-0.12.0 > /dev/null 2>&1 && 
-echo "Environment ready."
+echo "Initials ready."
 #git clone system craftsman strimzi training
